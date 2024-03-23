7faee954a47055319847cc1cbe9da29cf29b4abf
Кирилл Дегтярев <degtiarev.kd@phystech.edu>
2024-03-23 15:34:26 +0300

    cmake folder

---
+++ cmake/FindSPARSKIT.cmake
@@ -0,0 +1,6 @@
+find_library( SKIT_LIBRARY
+    NAMES skit
+    PATHS /Users/kirilldegtarev/Documents/lib/SPARSKIT2
+    NO_SYSTEM_ENVIRONMENT_PATH
+    NO_CMAKE_SYSTEM_PATH )
+message (${SKIT_LIBRARY})
